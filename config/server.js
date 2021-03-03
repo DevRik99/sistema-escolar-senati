5d29a03893dd756447b46389b028b88dc70c470e
Erik Jaime Pacheco Arana <devrikoficial@gmail.com>
2021-03-02 19:30:35 -0500

    Estructura inicial '

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c0b9ff478890992c4afcd728815e91b9'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ed3f1fca-8dfb-4ad6-912c-88f8af735de7'
+};
